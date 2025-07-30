12970b71750705721789ce021ee1e9e8fb104826
Rashnan <rashnan.m.s@gmail.com>
2025-07-30 17:45:37 +0500

    make stuff easier for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ import postcssNested from 'postcss-nested'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
+  // base: '/crypto',
   plugins: [
     vue({...templateCompilerOptions})
   ],
